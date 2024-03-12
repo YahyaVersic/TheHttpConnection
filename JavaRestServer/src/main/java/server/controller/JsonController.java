72e1530966348219a85c75b4f26ba4336abde6bf
Yahya Versic <Yahya.Versic@gmail.com>
2024-03-12 01:19:45 +0100

    Added a basic get request in JsonController

--- JavaRestServer/src/main/java/server/controller/JsonController.java
+++ JavaRestServer/src/main/java/server/controller/JsonController.java
@@ -1,12 +1,27 @@
 package server.controller;
 
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class JsonController
-{
+import java.util.ArrayList;
+import java.util.List;
 
+
+@RestController
+public class JsonController {
+
+    public JsonController() {
+
+    }
+
+    @GetMapping("/json")
+    public ResponseEntity<List<String>> getJson() {
+        var a = new ArrayList<String>(List.of("Hola", "Dos", "Tres", ":)"));
+        return new ResponseEntity<>(a, HttpStatus.valueOf(200));
+    }
 
 
 }
